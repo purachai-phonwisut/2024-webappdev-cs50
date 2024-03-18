fdc8e29207fa506c1be244ac043496b291b42e8e
purachai-phonwisut <151113523+purachai-phonwisut@users.noreply.github.com>
2024-03-18 23:54:12 +0700

    initial commit

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
+  document.querySelector('#compose-form').addEventListener('submit',send_email);
 
   // By default, load the inbox
   load_mailbox('inbox');
@@ -30,4 +31,25 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+}
+
+function send_email() {
+
+  let body = document.querySelector('#compose-body')
+  let recipients = document.querySelector('#compose-recipients')
+  let subject = document.querySelector('#compose-subject')
+
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+        recipients: recipients,
+        subject: subject,
+        body: body
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+      // Print result
+      console.log(result);
+  });
 }
